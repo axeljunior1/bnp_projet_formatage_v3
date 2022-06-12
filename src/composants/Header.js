f36283119cedd601ddeb4c98c783ae47f66fbcea
axeljunior1 <68278948+axeljunior1@users.noreply.github.com>
2022-06-12 11:52:49 +0200

    modif pour tester la prod

--- src/composants/Header.js
+++ src/composants/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <nav className='navbar py-5  navbar-expand-lg bg-light justify-content-center '>
-            <h1 className='display-1 '> <span className='cardifColorGreen'> <strong> Formater les articles de Cardif </strong></span></h1>
+            <h1 className='display-1 '> <span className='cardifColorGreen'> <strong> Formater les articles </strong></span></h1>
         </nav>
     );
 };
